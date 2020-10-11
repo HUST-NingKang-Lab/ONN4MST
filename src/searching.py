a09bcc0fc1793492ec2dcc171219dca5c7a6ba0d
HugoZha <hugozha@hust.edu.cn>
2020-10-11 11:39:49 +0800

    Sync

--- src/searching.py
+++ src/searching.py
@@ -14,12 +14,15 @@ from gen_ontology import get_biome_source
 from utils import *
 from predicting import *
 
+def get_gid(gid):
+  os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+  os.environ["CUDA_VISIBLE_DEVICES"]="{}".format(gid)
 
 def main():
   #get args
   parser = get_parser()
   args = parser.parse_args()
-  gpus, ifn, ofn, ontology, mdl, threshold, sf, gid, ofmt = args.gpus, args.ifn, args.ofn, args.tree, args.model, args.threshold, args.selfea, args.gpu_core_id, args.outfmt
+  gpus, ifn, ofn, ontology, mdl, threshold, sf, gid, mp, ofmt = args.gpus, args.ifn, args.ofn, args.tree, args.model, args.threshold, args.selfea, args.gpu_core_id, args.mapping, args.outfmt
   matrices_size,label_size = get_size(sf)
   if(gpus == 1):
     print("gpu mode...")
@@ -35,14 +38,14 @@ def main():
   if(ofmt ==  1):
     print("the output format is 1...")
     y_pred = threshold_process(threshold,y_pred)
-    res2txt_mode1(threshold,y_pred,unknown,ontology,ofn)
+    res2txt_mode1(threshold,y_pred,unknown,ontology,mp,ofn)
   if(ofmt == 2):
     print("the output format is 2...")
     y_pred = threshold_process(threshold,y_pred)
-    res2txt_mode2(threshold,y_pred,unknown,ontology,ofn)
+    res2txt_mode2(threshold,y_pred,unknown,ontology,mp,ofn)
   if(ofmt == 3):
     print("the output format is 3...")
-    res2txt_mode3(threshold,y_pred,unknown,ontology,ofn)
+    res2txt_mode3(threshold,y_pred,unknown,ontology,mp,ofn)
   print("Done!")
 if(__name__ == '__main__'):
   main()
